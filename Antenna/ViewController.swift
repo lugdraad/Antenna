f6a5343851ac443e36665d089896a2f091a976a5
Jamie <lugdraad@users.noreply.github.com>
2015-11-21 15:25:46 +1100

    Initial commit

---
+++ Antenna/ViewController.swift
@@ -0,0 +1,179 @@
+import UIKit
+import AVKit
+
+class ViewController: UIViewController {
+    
+    ///////////////////////////////////////////////////////////////
+    /// Set your options here /////////////////////////////////////
+    
+    let nearestCity = City.Sydney
+    // Capitals: Adelaide, Brisbane, Melbourne, Perth, Sydney
+    // QLD Regional: Cairns, Mackay, Maroochydore (Sunshine Coast),
+    //               Maryborough (Wide Bay), Rockhampton (Central),
+    //               Toowoomba, Townsville
+    
+    let streamQuality = Quality.High
+    // Auto, Low, Medium, High
+    
+    /// That is all ///////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////
+    
+    
+    var playerLayer = AVPlayerLayer()
+    var urlAddress = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Register for background / foreground notifications
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "handleEnteredBackground", name: UIApplicationDidEnterBackgroundNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "handleBecameFrontmost", name: UIApplicationDidBecomeActiveNotification, object: nil)
+        urlAddress = createURL()
+    }
+    
+    // Unregister for notifications
+    deinit {
+        NSNotificationCenter.defaultCenter().removeObserver(self)
+    }
+
+    // Handle a foreground notification
+    func handleBecameFrontmost() {
+        let videoURL = NSURL(string: urlAddress)
+        let playerItem = AVPlayerItem(URL: videoURL!)
+        let player = AVPlayer(playerItem: playerItem)
+        playerLayer = AVPlayerLayer(player: player)
+        playerLayer.frame = self.view.frame
+        self.view.layer.addSublayer(playerLayer)
+        player.play()
+    }
+    
+    // Handle a background notification
+    func handleEnteredBackground() {
+        playerLayer.player!.pause()
+        playerLayer.player = nil
+        playerLayer.removeFromSuperlayer()
+    }
+    
+    enum City {
+        case Adelaide, Brisbane, Cairns, Mackay, Maroochydore, Maryborough, Melbourne, Perth, Rockhampton, Sydney, Toowoomba, Townsville
+    }
+    
+    enum Quality {
+        case Auto, Low, Medium, High
+    }
+    
+    func createURL() -> String {
+        #if ABCNEWS24
+            switch streamQuality {
+            case .High:
+                return "http://iphonestreaming.abc.net.au/news24/news24_hi.m3u8"
+            case .Medium:
+                return "http://iphonestreaming.abc.net.au/news24/news24_med.m3u8"
+            case .Low:
+                return "http://iphonestreaming.abc.net.au/news24/news24_lo.m3u8"
+            default:
+                return "http://www.abc.net.au/res/streaming/video/hls/news24.m3u8"
+            }
+
+        #else
+            var result = "https://sevenwestmedia01-i.akamaihd.net/hls/live/"
+            #if CHANNELSEVEN
+                switch nearestCity {
+                case .Adelaide:
+                    result += "224816/ADE1"
+                case .Brisbane:
+                    result += "224815/BRI1"
+                case .Cairns:
+                    result += "224818/CNS1"
+                case .Mackay:
+                    result += "224820/MKY1"
+                case .Maroochydore:
+                    result += "224823/SSC1"
+                case .Maryborough:
+                    result += "224822/WBY1"
+                case .Melbourne:
+                    result += "224813/MEL1"
+                case .Perth:
+                    result += "224817/PER1"
+                case .Rockhampton:
+                    result += "224821/RKY1"
+                case .Sydney:
+                    result += "224814/SYD1"
+                case .Toowoomba:
+                    result += "224824/TWB1"
+                case .Townsville:
+                    result += "224819/TSV1"
+                }
+            #elseif CHANNELSEVENTWO
+                switch nearestCity {
+                case .Adelaide:
+                    result += "224829/ADE2"
+                case .Brisbane:
+                    result += "224828/BRI2"
+                case .Cairns:
+                    result += "224831/CNS2"
+                case .Mackay:
+                    result += "224833/MKY2"
+                case .Maroochydore:
+                    result += "224836/SSC2"
+                case .Maryborough:
+                    result += "224835/WBY2"
+                case .Melbourne:
+                    result += "224826/MEL2"
+                case .Perth:
+                    result += "224830/PER2"
+                case .Rockhampton:
+                    result += "224834/RKY2"
+                case .Sydney:
+                    result += "224827/SYD2"
+                case .Toowoomba:
+                    result += "224837/TWB2"
+                case .Townsville:
+                    result += "224832/TSV2"
+                }
+            #elseif CHANNELSEVENMATE
+                switch nearestCity {
+                case .Adelaide:
+                    result += "224842/ADE3"
+                case .Brisbane:
+                    result += "224841/BRI3"
+                case .Cairns:
+                    result += "224844/CNS3"
+                case .Mackay:
+                    result += "224846/MKY3"
+                case .Maroochydore:
+                    result += "224849/SSC3"
+                case .Maryborough:
+                    result += "224848/WBY3"
+                case .Melbourne:
+                    result += "224839/MEL3"
+                case .Perth:
+                    result += "224843/PER3"
+                case .Rockhampton:
+                    result += "224847/RKY3"
+                case .Sydney:
+                    result += "224840/SYD3"
+                case .Toowoomba:
+                    result += "224850/TWB3"
+                case .Townsville:
+                    result += "224845/TSV3"
+                }
+            #elseif RACINGDOTCOM
+                result += "224825/MISC1"
+            #endif
+            
+            switch streamQuality {
+            case .High:
+                result += "/master_high.m3u8"
+            case .Medium:
+                result += "/master_med.m3u8"
+            case .Low:
+                result += "/master_low.m3u8"
+            default:
+                result += "/master.m3u8"
+            }
+            
+            return result
+        #endif
+    }
+    
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+## Antenna
+
+Based on [Your Own Channel](https://github.com/kane2931/ownchannel), Antenna is a collection of side-loading tvOS apps for playing (the free and publicly available but geo-blocked) Australian commercial television streams on your Apple TV 4. The project is lacking any type of advanced user interface or EPG.
+
+Set your location inside the **ViewController.swift** file to view regional streams. As per the usual side-loading procedure, you will need to change the Bundle Identifier, however not just once but for each app target.
+
+All artwork (and initial testing) kindly provided by Steeley.
+
+Created as a learning project, please support the official apps when they become available.
